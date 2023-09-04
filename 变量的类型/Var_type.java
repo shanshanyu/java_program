33d8bedc9a1be26bde95f6df2b31cf80863042b7
shanshanyu <1525996190.qq.com>
2023-09-04 17:08:37 +0800

    new file

---
+++ 变量的类型/Var_type.java
@@ -0,0 +1,21 @@
+public class Var_type {
+	public static void main(String[] args) {
+		byte a = 1;
+		short b = 2;
+		int c = 3;
+		long d = 4;
+		float e = 1.2f;
+		double f = 1.3d;
+		boolean g = true;
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(g);
+		
+		
+	}
+}
